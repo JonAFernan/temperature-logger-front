a90b5bff1c350bbfa4ad0d75592958532e457c6b
Jon A.Fernadez <jafernandiaz@gmail.com>
2025-05-01 21:41:08 +0200

    data Simulator created

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { startSimulator } from './simulator/simulator.js';
+
+startSimulator();
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
---
+++ src/simulator/simulator.js
@@ -0,0 +1,61 @@
+//Formato compatible con la base de datos
+const formatDate = (date) => {
+    return date.split('.')[0] + '+00:00';
+};
+
+//Función para generar temperaturas aleatorias basadas en el setpoint
+function getRandomTemperature(max, temperature) {
+    const a = Math.random();
+    const increaseOrDecrease = a < 0.5 ? -1 : 1;
+    return parseFloat(
+        (temperature + Math.random() * max * increaseOrDecrease).toFixed(1),
+    );
+}
+
+//Función para obtener sensores desde el backend
+const fetchSensors = async () => {
+    try {
+        const response = await fetch('http://localhost:3000/sensors/all');
+        if (!response.ok) throw new Error('Error obteniendo sensores');
+        const sensors = await response.json();
+        return sensors;
+    } catch (error) {
+        console.error('Error al cargar sensores:', error);
+        return [];
+    }
+};
+
+//Función para generar datos de los sensores
+const generateSensorData = (sensors) => {
+    return sensors.map((sensor) => ({
+        address: sensor.address,
+        temperature: getRandomTemperature(2, sensor.setpoint),
+        date: formatDate(new Date().toISOString()),
+    }));
+};
+
+// Función para enviar datos al backend
+const sendDataToBackend = async (data) => {
+    try {
+        const response = await fetch('http://localhost:3000/records/add', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+        });
+
+        if (!response.ok) throw new Error('Error enviando datos');
+        console.log('Datos enviados correctamente.');
+    } catch (error) {
+        console.error(' Error al enviar datos:', error);
+    }
+};
+
+export const startSimulator = async () => {
+    console.log('simulator running');
+    const sensors = await fetchSensors();
+
+    setInterval(() => {
+        const newData = generateSensorData(sensors);
+        sendDataToBackend(newData);
+    }, 60000);
+};
